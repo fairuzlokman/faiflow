32e6fea0585dfdfade66ba1db18160bd92f8c121
fairuzlokman <fairuzlokman99@gmail.com>
2026-05-15 17:57:29 +0800

    update editor and prettier settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,23 @@
+import CanvasEditorView from '@/views/CanvasEditorView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+	{
+		path: '/',
+		name: 'canvas',
+		component: CanvasEditorView,
+	},
+	{
+		path: '/node/:id',
+		name: 'node',
+		component: CanvasEditorView,
+	},
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+	// history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHistory(),
+	routes,
 })
 
 export default router
---
+++ src/views/CanvasEditorView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+	import { useRouter } from 'vue-router'
+
+	const router = useRouter()
+
+	const handleClickMe = () => {
+		router.push({ name: 'node', params: { id: '123' } })
+	}
+</script>
+
+<template>
+	<h1>Hello there!</h1>
+	<button @click="handleClickMe">Click me!</button>
+</template>
+
+<style scoped>
+	@import '@vue-flow/core/dist/style.css';
+	@import '@vue-flow/core/dist/theme-default.css';
+</style>
